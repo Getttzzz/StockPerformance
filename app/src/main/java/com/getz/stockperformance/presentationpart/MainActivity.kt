04c968eb1112b71731d6bab3e56eaec575cb2f25
Yuriy Getsko <getttzzz@gmail.com>
2020-08-15 11:59:46 +0300

    Rename packages, add koin, add Application class.

---
+++ app/src/main/java/com/getz/stockperformance/presentationpart/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.getz.stockperformance.presentationpart
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+import com.getz.stockperformance.R
+import com.google.android.material.tabs.TabLayout
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    val mainViewModel by viewModel<MainViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setupViewPager()
+    }
+
+    private fun setupViewPager() {
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tabs)
+        tabs.setupWithViewPager(viewPager)
+    }
+}
---
+++ app/src/main/java/com/getz/stockperformance/presentationpart/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.getz.stockperformance.presentationpart
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+}
