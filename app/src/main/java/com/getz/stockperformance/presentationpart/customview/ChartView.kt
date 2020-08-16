f009c9c9b96b2b2203fc113234746cefa65c2f37
YuriyGetsko <getttzzz@gmail.com>
2020-08-16 19:11:06 +0300

    Finish drawing of AAPL stocks for one month. Add unit tests to check max and min values of Y axis.

--- app/src/main/java/com/getz/stockperformance/presentationpart/customview/ChartView.kt
+++ app/src/main/java/com/getz/stockperformance/presentationpart/customview/ChartView.kt
@@ -15,15 +15,17 @@ class ChartView(
 
     private val stocks: MutableList<Stock> = mutableListOf()
 
-    private var xMin = 0
-    private var xMax = 0
     private var yMin = 0
     private var yMax = 0
+    private var topValueAxisY = 0
+    private var bottomValueAxisY = 0
+    private var diffTopBottomAxisY = 0
 
     private val stockPointPaint = Paint().apply {
         color = Color.GRAY
         strokeWidth = 5f
         style = Paint.Style.STROKE
+        isAntiAlias = true
     }
 
     private val stockLinePaint = Paint().apply {
@@ -39,53 +41,55 @@ class ChartView(
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        println("GETTTZZZ.ChartView.onDraw ---> view width=$width height=$height")
-        println("GETTTZZZ.ChartView.onDraw ---> stocks=${stocks}")
 
-
-        // Apple stocks for month (20-21 working day)
+        // Apple stocks for month (21 working day)
         if (stocks.isNotEmpty()) {
             val appleStocks = stocks[0]
 
-            var progressiveX = 0f
+            var progressiveX = ((width / appleStocks.valuesMap.size).toFloat()) / 2
             val progressiveStep = width / appleStocks.valuesMap.size
-            println("GETTTZZZ.ChartView.onDraw ---> progressiveStep=$progressiveStep")
 
             appleStocks.valuesMap.entries.forEachIndexed { index, entry ->
-                println("GETTTZZZ ---> i=$index key=${entry.key} value=${entry.value}")
 
                 if (index < appleStocks.valuesMap.size - 1) {
                     val nextPoint = appleStocks.valuesMap.values.toMutableList()[index + 1]
 
                     val startX = progressiveX
-                    val startY = entry.value.toFloat()
+                    val startY = entry.value.toRealY()
                     val endX = progressiveX + progressiveStep
-                    val endY = nextPoint.toFloat()
+                    val endY = nextPoint.toRealY()
                     canvas.drawLine(startX, startY, endX, endY, stockLinePaint)
                 }
 
-
                 // todo add text above each circle "328.2 (+1.7%)"
-                canvas.drawCircle(progressiveX, entry.value.toFloat(), 10f, stockPointPaint)
+                canvas.drawCircle(progressiveX, entry.value.toRealY(), 10f, stockPointPaint)
 
                 progressiveX += progressiveStep
             }
         }
     }
 
+    // todo calculate one stock (AAPL)
+    // todo after that, add others to the chart.
+    // todo Figure out max and min Y values among three maps of stocks.
+
     fun setupData(stocks: MutableList<Stock>) {
         println("GETTTZZZ.ChartView.setupData ---> stocks.size=${stocks.size}")
 
-        xMin = 0
-        xMax = 10
         yMin = stocks.get(0).valuesMap.values.min()?.toInt() ?: 0
         yMax = stocks.get(0).valuesMap.values.max()?.toInt() ?: 0
+        topValueAxisY = yMax.roundToTens(true)
+        bottomValueAxisY = yMin.roundToTens(false)
+        diffTopBottomAxisY = topValueAxisY - bottomValueAxisY
+
+        println("GETTTZZZ.ChartView.setupData ---> yMin=$yMin yMax=$yMax topValueAxisY=$topValueAxisY bottomValueAxisY=$bottomValueAxisY diffTopBottomAxisY=$diffTopBottomAxisY")
 
         this.stocks.clear()
         this.stocks.addAll(stocks)
         invalidate()
     }
 
-    private fun Int.toRealX() = this.toFloat() / xMax * width
-    private fun Double.toRealY() = this.toFloat() / yMax * height
+    private fun Double.toRealY() = (topValueAxisY - this.toFloat()) * height / diffTopBottomAxisY
 }
+
+internal fun Int.roundToTens(above: Boolean) = (if (above) this + 10 else this - 10) - (this % 10)
--- app/src/test/java/com/getz/stockperformance/ExampleUnitTest.kt
+++ app/src/test/java/com/getz/stockperformance/ExampleUnitTest.kt
@@ -1,17 +1,42 @@
 package com.getz.stockperformance
 
+import com.getz.stockperformance.presentationpart.customview.roundToTens
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
-import org.junit.Assert.*
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun transformMaxYToChartAxisTopText() {
+        // todo 393 -> 400
+        // todo 390 -> 400
+        // todo 399 -> 400
+
+        val inputList = listOf(393, 390, 399)
+        val resultList = inputList.map {
+            val result = it.roundToTens(true)
+            result
+        }
+
+        resultList.forEach {
+            assertEquals(400, it)
+        }
+    }
+
+    @Test
+    fun transformMinYToChartAxisBottomText() {
+        // todo 353 -> 340
+        // todo 350 -> 340
+        // todo 359 -> 340
+
+        val inputList = listOf(353, 350, 359)
+        val resultList = inputList.map {
+            val result = it.roundToTens(false)
+            result
+        }
+
+        resultList.forEach {
+            assertEquals(340, it)
+        }
     }
 }
