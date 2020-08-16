32b4875d0634bd6db99e03358d2ee1a3f4fb059e
YuriyGetsko <getttzzz@gmail.com>
2020-08-16 20:07:00 +0300

    Add start and bottom paddings for chart.

--- app/src/main/java/com/getz/stockperformance/presentationpart/customview/ChartView.kt
+++ app/src/main/java/com/getz/stockperformance/presentationpart/customview/ChartView.kt
@@ -13,6 +13,11 @@ class ChartView(
     attributeSet: AttributeSet
 ) : View(context, attributeSet) {
 
+    companion object {
+        private const val START_PADDING = 120
+        private const val BOTTOM_PADDING = 120
+    }
+
     private val stocks: MutableList<Stock> = mutableListOf()
 
     private var yMin = 0
@@ -20,6 +25,7 @@ class ChartView(
     private var topValueAxisY = 0
     private var bottomValueAxisY = 0
     private var diffTopBottomAxisY = 0
+    private var heightWithPadding = 0
 
     private val stockPointPaint = Paint().apply {
         color = Color.GRAY
@@ -42,30 +48,41 @@ class ChartView(
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
+        heightWithPadding = height - BOTTOM_PADDING
+
         // Apple stocks for month (21 working day)
         if (stocks.isNotEmpty()) {
             val appleStocks = stocks[0]
 
-            var progressiveX = ((width / appleStocks.valuesMap.size).toFloat()) / 2
-            val progressiveStep = width / appleStocks.valuesMap.size
+            val viewWidthWithPadding = width - START_PADDING
+            var progressiveX = START_PADDING
+            val progressiveStepPx = viewWidthWithPadding / appleStocks.valuesMap.size
 
             appleStocks.valuesMap.entries.forEachIndexed { index, entry ->
-
                 if (index < appleStocks.valuesMap.size - 1) {
                     val nextPoint = appleStocks.valuesMap.values.toMutableList()[index + 1]
 
-                    val startX = progressiveX
+                    val startX = progressiveX.toFloat()
                     val startY = entry.value.toRealY()
-                    val endX = progressiveX + progressiveStep
+                    val endX = (progressiveX + progressiveStepPx).toFloat()
                     val endY = nextPoint.toRealY()
+
                     canvas.drawLine(startX, startY, endX, endY, stockLinePaint)
                 }
 
-                // todo add text above each circle "328.2 (+1.7%)"
-                canvas.drawCircle(progressiveX, entry.value.toRealY(), 10f, stockPointPaint)
 
-                progressiveX += progressiveStep
+                // todo add text above each circle "328.2 (+1.7%)"
+                canvas.drawCircle(
+                    progressiveX.toFloat(),
+                    entry.value.toRealY(),
+                    10f,
+                    stockPointPaint
+                )
+
+                progressiveX += progressiveStepPx
             }
+
+
         }
     }
 
@@ -78,6 +95,9 @@ class ChartView(
 
         yMin = stocks.get(0).valuesMap.values.min()?.toInt() ?: 0
         yMax = stocks.get(0).valuesMap.values.max()?.toInt() ?: 0
+        //todo uncomment when I start scaling solution to work with arrays.
+        //yMin = getMinValueFromLists(stocks)
+        //yMax = getMaxValueFromLists(stocks)
         topValueAxisY = yMax.roundToTens(true)
         bottomValueAxisY = yMin.roundToTens(false)
         diffTopBottomAxisY = topValueAxisY - bottomValueAxisY
@@ -89,7 +109,22 @@ class ChartView(
         invalidate()
     }
 
-    private fun Double.toRealY() = (topValueAxisY - this.toFloat()) * height / diffTopBottomAxisY
+    private fun getMinValueFromLists(stocks: MutableList<Stock>): Int {
+        return stocks.map {
+            val min = it.valuesMap.values.min()?.toInt() ?: 0
+            min
+        }.min() ?: 0
+    }
+
+    private fun getMaxValueFromLists(stocks: MutableList<Stock>): Int {
+        return stocks.map {
+            val min = it.valuesMap.values.max()?.toInt() ?: 0
+            min
+        }.max() ?: 0
+    }
+
+    private fun Double.toRealY() =
+        (topValueAxisY - this.toFloat()) * (heightWithPadding) / diffTopBottomAxisY
 }
 
 internal fun Int.roundToTens(above: Boolean) = (if (above) this + 10 else this - 10) - (this % 10)
